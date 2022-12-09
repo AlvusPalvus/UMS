bd228e0d47fda06addb1544bbe89653bb805dff3
oi18aln <btu2Tvbg>
2022-12-09 20:07:25 +0100

    Contentful rich text editor added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import { createClient } from "contentful";
 import heroStyles from "../styles/Header.module.css";
 import Link from "next/link";
 import Header from "../components/Header";
+import { BLOCKS } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export async function getStaticProps() {
   const client = createClient({
@@ -12,41 +14,30 @@ export async function getStaticProps() {
     accessToken: process.env.CONTENTFUL_ACCESS_KEY as string,
   });
 
-  //const res = await client.getEntries({ content_type: "nyhet" });
+  const res = await client.getEntry("HDZTWw3xASIbiFDQOeh2W", { include: 4 });
+  console.log(res);
 
-  // const hero_img = await client.getEntry("4hiqPvQpvgfuZmOFgPcrtl");
-  // const res = await client.getEntries()
-  // console.log(res)
-
-  // const hero_img = await client.getAsset("4hiqPvQpvgfuZmOFgPcrtl")
-
-  //Hämta alla assets med taggen header
-  const headerAssets = await client.getAssets({
-    "metadata.tags.sys.id[in]": "header",
-  });
-  const hero_src = headerAssets.items[1].fields.file.url;
-
-  console.log(hero_src);
+  const hero_src = res.fields.hero.fields.file.url;
+  const page_title = res.fields.pageTitle;
+  const sections = res.fields.sections;
 
   return {
     props: {
       hero_src,
+      page_title,
+      sections,
     },
   };
 }
 
-export default function Home({ nyheter, res, hero_src }) {
-  console.log(res);
-  console.log(hero_src);
+export default function Home({ hero_src, page_title, sections }) {
   return (
     <>
       <Header
         heroSrc={hero_src}
         heroContent={
           <div>
-            <h1>Gå med i UMS</h1>
-            <p>Umeå Medicinska studentkår .....</p>
-            <button> Bli medlem! </button>
+            <h1>{page_title}</h1>
           </div>
         }
       />
@@ -57,3 +48,16 @@ export default function Home({ nyheter, res, hero_src }) {
     </>
   );
 }
+
+/*
+const options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ENTRY]: (node) => {
+      const { title, description } = node.data.target.fields;
+      return <Button title={title} description={description} />
+    }
+  }
+};
+*/
+
+// documentToReactComponents(document, options); // Används för rich text

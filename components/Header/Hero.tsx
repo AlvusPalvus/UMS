b7964af0375cc2aa4cca387d1a0a867dcaddba8b
oi18aln <btu2Tvbg>
2023-02-19 09:53:04 +0100

    update

--- components/Header/Hero.tsx
+++ components/Header/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = ({ heroImage, heroContent }: HeroType) => {
             {/* Overlay */}
             <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2] h-full" />
             <div className="p-5 text-white z-[2] mt-[10rem] mb-[10rem]">
-                <ReactMarkdown className={"heroContent container"}>
+                <ReactMarkdown className={"heroContent container buttonHome"}>
                     {heroContent}
                 </ReactMarkdown>
             </div>

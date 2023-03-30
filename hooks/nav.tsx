b7e0bd04b262b36c86b307af088a711a8842b6a9
Jonathan Smedborn <jonathan.smedborn@gmail.com>
2023-03-30 22:30:12 +0200

    Move scrollBase styling to hook

--- hooks/nav.tsx
+++ hooks/nav.tsx
@@ -10,3 +10,38 @@ export const useCurrentNav = () => {
         return { currentLink, currentSubLink };
     }, [router.pathname, router.asPath])
 }
+
+
+export const useNavScrollStyling = () => {
+    const [scrolled, setScrolled] = useState(false);
+    useEffect(() => {
+        const colorChange = () => {
+            if (window.scrollY >= 90) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+        colorChange();
+        window.addEventListener("scroll", colorChange);
+        return () => window.removeEventListener("scroll", colorChange);
+    }, []);
+
+    // Scrollbased styling
+
+    let backgroundColor = scrolled ? "bg-white" : "bg-transparent";
+    let textColor = scrolled ? "text-black" : "text-white";
+    let dropDownBgColor = scrolled ? " bg-white" : " bg-black ";
+    let hoverColor = scrolled
+        ? " hover:bg-primaryGreen hover:text-white "
+        : " hover:bg-white  hover:text-black ";
+    let margin = scrolled ? "" : "m-4";
+    return {
+        backgroundColor,
+        textColor,
+        dropDownBgColor,
+        hoverColor,
+        margin,
+        scrolled
+    };
+}

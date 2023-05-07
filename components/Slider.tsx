0046f65e807d280c900cf0af76fa5a400f826173
Jonathan Smedborn <jonathan.smedborn@gmail.com>
2023-05-07 16:00:41 +0200

    Remove unused divs

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -6,15 +6,15 @@ type Props = { news: NewsCardType[] }
 
 const Slider = ({ news }: Props) => {
     return (
-        <div className="bg-accent-100 border-black border">
-            <div className="container  h-fit overflow-hidden bg-white bg-opacity-70">
-                <div className="h-fit bg-white flex p-2 gap-2 lg:p-8 lg:gap-8 overflow-x-scroll lg:my-12 lg:mx-8 ">
-                    {news.map((card) => (
-                        <NewsCard card={card} width={300} key={card.slug} />
-                    ))}
-                </div>
-            </div>
+        // <div className="bg-accent-100">
+        // <div className="container  h-fit overflow-hidden bg-white bg-opacity-70">
+        <div className="h-fit bg-white flex flex-1 gap-2 lg:gap-8 overflow-x-scroll ">
+            {news.map((card) => (
+                <NewsCard card={card} width={300} key={card.slug} />
+            ))}
         </div>
+        // </div>
+        // </div>
     )
 }
 

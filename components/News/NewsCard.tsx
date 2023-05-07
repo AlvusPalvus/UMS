441b7f79bfd6a69e5b34aa4121748f1b021fca2e
Jonathan Smedborn <jonathan.smedborn@gmail.com>
2023-05-07 18:31:10 +0200

    Fix flex responsive design

--- components/News/NewsCard.tsx
+++ components/News/NewsCard.tsx
@@ -1,34 +1,28 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import React from "react";
 import { FiChevronRight } from "react-icons/fi";
-import ReactMarkdown from "react-markdown";
 import { NewsCard } from "../../types/Topics";
 import Gallery from "../Gallery";
 
 type Props = {
     card: NewsCard;
     width: number;
+    index: number
 };
 
-const NewsCard = ({ card, width }: Props) => {
-    const router = useRouter();
-    console.log(card.preview);
+const NewsCard = ({ card, width, index }: Props) => {
+
 
     return (
-        <Link className="cursor-pointer" href={"/news/" + card.slug}>
-            <div className="card bg-neutral-100 link py-4 flex flex-col gap-1.5 h-[400px] lg:h-[600px] max-w-[500px] min-w-[90%] md:min-w-[40%] xl:min-w-[30%]">
-                <div className="flex flex-col md:flex-nowrap md:gap-3 justify-between items-start pb-1 px-2">
-                    <span className="text-sm text-gray">{card.date}</span>
-                    <h3 className="h3 p-0 truncate">{card.heading}</h3>
-                </div>
+        <Link id={`news-index-${index}`} className="cursor-pointer shrink-0 lg:basis-2/6  md:basis-1/2 basis-full snap-start" href={"/news/" + card.slug}>
+            <div className="card bg-neutral-100 link p-4 flex flex-col gap-1.5 h-full">
 
-                <ReactMarkdown
-                    className={"fw-regular px-2"}
-                    linkTarget="_blank"
-                >
+                <span className="text-sm text-gray">{card.date}</span>
+                <h3 className="h3 whitespace-pre-wrap p-0 ">{card.heading}</h3>
+                <p>
                     {card.preview}
-                </ReactMarkdown>
+                </p>
+
                 {card.image ? (
                     <div className="relative flex-grow m-4 ">
                         <Gallery
@@ -43,7 +37,7 @@ const NewsCard = ({ card, width }: Props) => {
                 )}
                 <div className=" cursor-pointer flex flex-nowrap items-center self-end">
                     <span>Läs mer</span>
-                    <FiChevronRight className="text-2xl pt-1" />
+                    <FiChevronRight className="text-2xl" />
                 </div>
             </div>
         </Link>

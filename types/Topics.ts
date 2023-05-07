4954ba84d2442be42f6a06692061ff9c09b39782
Jonathan Smedborn <jonathan.smedborn@gmail.com>
2023-05-07 16:34:19 +0200

    Add richtext to body instead of markdown

--- types/Topics.ts
+++ types/Topics.ts
@@ -2,23 +2,23 @@ import { Document } from "@contentful/rich-text-types";
 import { CfImage, CfLink } from "./Elements";
 
 export type Field = {
-    displayType: Card | PlainText | Accordion;
-    slug: string;
-    heading?: string;
-    body: Document;
-    buttons?: CfLink[];
-    assets: string[];
-    backgroundColor?: string;
+  displayType: Card | PlainText | Accordion;
+  slug: string;
+  heading?: string;
+  body: Document;
+  buttons?: CfLink[];
+  assets: string[];
+  backgroundColor?: string;
 };
 
 export type Gallery = {
-    heading: string;
-    images: CfImage[];
+  heading: string;
+  images: CfImage[];
 };
 
 export type ImageComponent = {
-    image: CfImage;
-    displaySettings: string;
+  image: CfImage;
+  displaySettings: string;
 };
 
 export type Card = "Card";
@@ -26,47 +26,47 @@ export type PlainText = "Plain Text";
 export type Accordion = "Accordion";
 
 export type Contact = {
-    heading?: string;
-    text?: string;
-    contactItems: ContactItem;
+  heading?: string;
+  text?: string;
+  contactItems: ContactItem;
 };
 
 export type Person = {
-    name: string;
-    role: string;
-    profileImage: CfImage;
-    contact: Contact;
+  name: string;
+  role: string;
+  profileImage: CfImage;
+  contact: Contact;
 };
 
 export type ContactItem = {
-    email?: string;
-    phone?: string;
-    adress?: string;
-    facebook?: string;
-    instagram?: string;
-    adressLink?: string;
-    facebookLink?: string;
-    instagramLink?: string;
+  email?: string;
+  phone?: string;
+  adress?: string;
+  facebook?: string;
+  instagram?: string;
+  adressLink?: string;
+  facebookLink?: string;
+  instagramLink?: string;
 };
 
 export type NewsCard = {
-    slug: string;
-    heading: string;
-    date: string;
-    body: string;
-    preview: string;
-    image?: CfImage;
+  slug: string;
+  heading: string;
+  date: string;
+  body: Document;
+  preview: string;
+  image?: CfImage;
 };
 export type EventCard = {
-    slug: string;
-    heading: string;
-    date: string;
-    time: string;
-    body: string;
-    image: CfImage;
-    host?:
-        | "Kårstyrelsen"
-        | "Festmästeriet"
-        | "Sportmästeriet"
-        | "Kårhuset Villan";
+  slug: string;
+  heading: string;
+  date: string;
+  time: string;
+  body: string;
+  image: CfImage;
+  host?:
+    | "Kårstyrelsen"
+    | "Festmästeriet"
+    | "Sportmästeriet"
+    | "Kårhuset Villan";
 };
